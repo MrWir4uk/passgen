e27b0ffdbffcc015bc5cf59da5171fc31462a3b8
MrWir4uk <152081630+MrWir4uk@users.noreply.github.com>
2024-02-11 17:43:24 +0200

    blocnot

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from ui import Ui_MainWindow
-import secrets
-class PassGenerator(QMainWindow):
+
+class Widget(QMainWindow):
     def   __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.generate_pass)
+        self.notes = {
+            "Перша замітка":{
+                "текст": "Це текст першої замітки",
+                "теги": []
 
-    def generate_pass(self):
-        password = secrets.token_hex(8)
-        self.ui.password_label.setText(password)
-        with open("my_pass.txt", "w") as file:
-            file.write('\n'+password)
-        
-
-
-
+            }
+        }
+        self.ui.vikno_zamit.addItems(self.notes)
 app = QApplication([])
-ex = PassGenerator()
+ex = Widget()
 ex.show()
 app.exec_()
