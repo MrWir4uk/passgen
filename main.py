e06d323eb9e064fe210ca5b1ed024d1d6da842ed
MrWir4uk <152081630+MrWir4uk@users.noreply.github.com>
2024-02-18 17:31:55 +0200

    new project

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from ui import Ui_MainWindow
@@ -15,6 +16,19 @@ class Widget(QMainWindow):
             }
         }
         self.ui.vikno_zamit.addItems(self.notes)
+        self.ui.vikno_zamit.itemClicked.connect(self.show_note)
+        self.ui.save_btn.clicked.connect(self.save_note)
+
+    def show_note(self):
+        name = self.ui.vikno_zamit.selectedItems()[0].text()
+        self.ui.title_edit.setText(name)
+        self.ui.text_edit.setText(self.notes[name]["текст"])
+
+    def save_note(self):
+        self.notes[self.ui.title_edit.text()] = self.ui.text_edit.toPlainText()
+        with open("notes.json", "w", encoding="utf-8") as file:
+            json.dump(self.notes, file)
+
 app = QApplication([])
 ex = Widget()
 ex.show()
